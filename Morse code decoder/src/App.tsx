4dc7b3b7b2d96ba39340860038066cd776dfec26
Jakub Drzewiecki <kuba.drzewiecki@icloud.com>
2021-09-02 02:31:33 +0200

    Created a screen and moved functions to class

--- Morse code decoder/src/App.tsx
+++ Morse code decoder/src/App.tsx
@@ -4,25 +4,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  state = {word: ''}
   render() {
+    const word = this.state['word'];
       return (
         <div id="container">
           <div id='light' className='off'></div>
-          {/* <Screen /> */}
-          <button id="morseButton" onMouseDown={lightOn} onMouseUp={lightOff} ></button>
+          <div id="screen">{word}</div>
+          <button id="morseButton" 
+          onMouseDown={this.mouseDownFn} 
+          onMouseUp={this.mouseUpFn}></button>
         </div>
       );
+  };
+  mouseDownFn() {
+    const light = document.getElementById('light');
+    if (light instanceof HTMLElement){
+      light.className = 'on';};
+  };
+  mouseUpFn() {
+    const light = document.getElementById('light');
+    if (light instanceof HTMLElement){
+      light.className = 'off';};
   }
 };
-
-function lightOn() {
-  const light = document.getElementById('light');
-  if (light instanceof HTMLElement){
-    light.className = 'on';};
-}
-function lightOff() {
-  const light = document.getElementById('light');
-  if (light instanceof HTMLElement){
-    light.className = 'off';};
-}
 export default App;
