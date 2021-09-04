f8197facc0399edfbba17643200e830112b3b649
Jakub Drzewiecki <kuba.drzewiecki@icloud.com>
2021-09-04 02:15:24 +0200

    Added clear button, made a possibility to change text on screen

--- Morse code decoder/src/App.tsx
+++ Morse code decoder/src/App.tsx
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-  state = {word: ''}
+interface IProps {
+}
+
+interface IState {
+  word?: string;
+}
+
+class App extends React.Component<IProps, IState> {
+
+  constructor(props: {} | Readonly<{}>) {
+    super(props);
+    this.state = {
+      word: ""
+    };
+    this.mouseUpFn = this.mouseUpFn.bind(this);
+  }
+
   render() {
-    const word = this.state['word'];
       return (
         <div id="container">
           <div id='light' className='off'></div>
-          <div id="screen">{word}</div>
+          <div id="screen">{this.state.word}</div>
+          <button id="clearButton" onClick={() => {this.setState({word: ""})}}>Clear</button>
           <button id="morseButton" 
           onMouseDown={this.mouseDownFn} 
           onMouseUp={this.mouseUpFn}></button>
         </div>
       );
   };
+
   mouseDownFn() {
     const light = document.getElementById('light');
     if (light instanceof HTMLElement){
       light.className = 'on';};
   };
+
   mouseUpFn() {
     const light = document.getElementById('light');
     if (light instanceof HTMLElement){
       light.className = 'off';};
-  }
+    this.setState({word: this.state.word + 'a'})
+  };
 };
 export default App;
